c7b43ba50fe39f86cfacc743b92bb68f1196873c
Antonio Maciel <maciel.antoniofh@gmail.com>
2025-08-13 23:43:35 +0000

    mentoria

---
+++ mentoria01.py
@@ -0,0 +1,21 @@
+# print("Presente!")
+# var = int(input("'Informe um numero:"))
+# if var == 2:
+#     print("Entrou no if")
+# else:
+#     print("Não entrou no if")
+# soma = 0
+# while True:
+#     num = input("Informe os números para somar (0 finaliza a soma): "))
+#     if num.isalpha():
+#         print("Informe apenas números")
+#         continue
+#     if num  == "0":
+#         break
+#     soma += float(num)
+# list = {"banana","pera","maça"}
+# for fruta in range 
+# nome = "antonio"
+# idade = "segredo"
+# print("Meu nome é", nome, "eminha idade é", idade)
+# print(f"Meu nome é {nome} e minha idade é {idade}")

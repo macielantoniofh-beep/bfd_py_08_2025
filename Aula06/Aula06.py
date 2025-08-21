3c7b6dcd55af34b132067c5f924e079c10aa1750
Antonio Maciel <maciel.antoniofh@gmail.com>
2025-08-21 23:47:00 +0000

    -

---
+++ Aula06/Aula06.py
@@ -0,0 +1,59 @@
+# CRUD  - CREATE  READ  UPDATE  DELETE - DIVIDIR AS ETAPAS
+# CONSTRUÇÃO DE UM CRUD
+# BANCO DE DADOS
+# 
+# 
+# DICIONARIOS  -  CHAVE:VALOR - DINAMICO E ORDENADOS (ORDEM DE ADIÇÃO) - KEY:VALUE, KEY NAO PODE REPETIR
+# 
+# CRIANDO UM DICIONARIO
+# dic_vazio ={}
+# print(type(dic_vazio))
+# print(dic_vazio)
+# 
+# print(dic_vazio.["nome"])  PEGAR UM VALOR ATRIBUIDO A UMA CHAVE
+# print(dic_vazio.get("nome")) mesma função
+# 
+# print(dict.items()) LISTANDO TOSOS OS ITENS
+# 
+# print(dict.keys()) LISTAR VALORES DAS CHAVES
+# 
+# BANCO DE DADOS RELACIONAIS E NAO-RELACIONAIS
+# 
+# print(dict.values()) LISTANDO OS VALORES
+#
+#dict["CHAVEprofissao"] = "Desenvolvedor" ADICIONANDO ITENS DO DICIONARIO E VAI PARA O ULTIMO NAO PERMITE COLOCA NO INICIO, MEIO
+# dict.update({"idade":50}) MODIFICAR O VALOR
+# print(dict)
+#
+# dict.pop("idade")   REMOVENDO 
+# print(dict)
+# 
+# dict.popitem()
+# print(dict)
+# 
+# dict.clear() APAGA O DICIONARIO TODE E ENTREGA VAZIO
+# print(dict)
+# 
+# dict  
+# deldict["idade"]
+# 
+# 
+# dict2 = dict.copy() FORMA DE COPIAR DICIONARIO
+# print(f"Id de dict:{id(dict)}\nId de dict2:{id(dict2)}") 
+# 
+# for itens in dict.items():
+    #   print(itens)
+# for itens in dict.values():
+#     if itens == "antonio"
+#         print("Achamos o meliante")
+#
+#DEIXAR UM VIRGULA NO FINAL DO DICIONARIO PARA ADICIONAR SE PRECISAR
+# 
+# ALIMENTAÇÃO DO DICIONARIO PELA VARIAVEL
+# 
+# 
+# 
+# 
+# 
+# 
+# 

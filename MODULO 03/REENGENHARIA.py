388ab9326a948443cfbd1edf5afa29e3e1f88a14
Antonio Maciel <maciel.antoniofh@gmail.com>
2025-09-30 22:19:58 +0000

    reengenharia

---
+++ MODULO 03/REENGENHARIA.py
@@ -0,0 +1,29 @@
+'''
+manutenção de software legado
+
+-como solução
+nova documentação
+refatoração da arquitetura
+tradução dos programas para uma linguagem mais moderna
+modificação e atualização da estrutura e dos valores dos dados
+
+duas vantagens comparado a substituição
+menor risco
+menor custo
+
+processo - foto -
+
+Reengenharia de dados
+
+Qualidade de software
+5 perguntas
+atributos
+
+Gerenciamento de riscos
+tipos
+processos - identificação, analise, planejamento
+
+
+
+
+'''

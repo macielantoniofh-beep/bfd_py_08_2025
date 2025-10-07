ec511e1bae380b2d6343aaa6cab0a82b8d074b36
Antonio Maciel <maciel.antoniofh@gmail.com>
2025-10-07 22:35:20 +0000

    ass_agr_comp

---
+++ MODULO 03/aula19.py
@@ -0,0 +1,22 @@
+'''
+ANOTAÇÃO LAMBDA
+
+tipos de classes
+associação
+relação fraca --- temporaria
+como fazer no uml traço continuo e seta aberta
+
+agregação
+o objeto tem o outro, mas são independentes
+(uml)linha continua losango aberto para o maior
+
+composição
+mais forte
+classe "dona"
+
+objeto é criado na classe inferior
+o objeto compoe o outro objeto
+uml
+do objeto menor para a classe com linha continua e losango preenchido
+
+'''

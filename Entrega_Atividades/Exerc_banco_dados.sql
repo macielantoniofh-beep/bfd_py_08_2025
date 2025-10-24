bfdcbc81e786b852c806e8cfa864d3197a5df7e5
Antonio Maciel <maciel.antoniofh@gmail.com>
2025-10-24 16:51:07 +0000

    execbanco

---
+++ Entrega_Atividades/Exerc_banco_dados.sql
@@ -0,0 +1,20 @@
+--01
+--SELECT id_turma, COUNT () AS numero_alunos
+--FROM Aluno
+--GROUP BY id_turma
+
+-- 02
+-- SELECT *,  MIN (mensalidade) AS menor_mensalidade
+-- FROM Curso
+
+-- 03
+-- SELECT nome , MAX (nota1) AS maior_nota1
+-- FROM Aluno
+
+-- 04
+-- SELECT SUM (mensalidade)
+-- FROM Curso
+
+-- 05
+-- SELECT AVG (nota2) AS media
+-- FROM Aluno
